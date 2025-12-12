32d68112dee3c2b01c9f0dfb34705d016940773d
DilanEA12 <dilanechavarria68@gmail.com>
2025-12-12 16:07:14 -0500

    hola

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+  return (<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hamburguesa
         </p>
         <a
           className="App-link"
@@ -15,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Dilan Echavarria
         </a>
       </header>
     </div>
